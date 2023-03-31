2e2b8a7444a2d550b90d7e998a59e440713b6095
Wu Ze <zwubn@connect.ust.hk>
2023-03-31 01:22:16 +0000

    done

--- library.cpp
+++ library.cpp
@@ -23,6 +23,9 @@ condition_variable checkout_ready;
 */
 void books_checkout(int quantity)
 {
+    unique_lock<mutex> ul(mut);
+    checkout_ready.wait(ul,[&]{return books_available>quantity;});
+    books_available-=quantity;
 
 }
 
@@ -34,7 +37,10 @@ void books_checkout(int quantity)
  */
 void books_checkin(int quantity)
 {
-
+    unique_lock<mutex> ul(mut);
+    books_available+=quantity;
+    ul.unlock();
+    checkout_ready.notify_all();
 }
 
 /**
@@ -61,9 +67,16 @@ int main()
     thread *student_threads = new thread[6];
 
     /*TODO: Create the six student threads that will use the function (manager) for a respective number of books (cart[i])*/
+    for(int i=0;i<6;++i)
+    {
+        student_threads[i]=thread(manager,i,cart[i]);
+    }
 
     /*TODO: Wait for all the six threads*/
-
+    for(int i=0;i<6;++i)
+    {
+        student_threads[i].join();
+    }
     delete[] student_threads;
     return 0;
 }
